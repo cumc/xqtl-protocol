7c8e426d4dc396c3fc0bef9805978802f8f51227
Hao Sun <hsun3163@cumc.columbia.edu>
2023-09-16 10:11:40 -0400

    susie pipeline fix

--- code/misc/xqtl_utils.R
+++ code/misc/xqtl_utils.R
@@ -109,26 +109,55 @@ load_regional_association_data <- function(genotype, # PLINK file
                                            maf_cutoff = 0,
                                            mac_cutoff = 0,
                                            imiss_cutoff = 0,
-                                           y_as_matrix = FALSE) {
+                                           y_as_matrix = FALSE
+                                          ) {
+       library("plink2R") # This is to add Rcpp and other dependency
     library("dplyr")
     library("readr")
     library("stringr")
     library("purrr")
- 
+
     ### Load genotype
-    geno = plink2R::read_plink(genotype)
+    geno = read_plink(genotype)
     rownames(geno$bed) = read.table(text = rownames(geno$bed), sep= ":" )$V2
-    ### FIXME: complete this -- let's just summarize genotype data is enough
-    print("Dimension of input genotype data is")
+    
+    
+     ### Load phenotype and covariates
+    phenotype_list = tibble(covariate_path = covariate ,phenotype_path =phenotype ) %>%
+        mutate(covar = map(covariate_path, ~read_delim(.x,"\t")%>%select(-1)%>%na.omit%>%t()),
+              Y = map2(phenotype_path,covar, ~read_delim(.x,"\t")%>%select(-4)%>%select(rownames(.y))%>%t()%>%as.matrix), # Y and covar 1st match
+              Y = map(Y, ~.x%>%na.omit),    # remove na where Y raw data has na which block regression
+              dropped_sample = map2(covar, Y , ~rownames(.x)[!rownames(.x) %in% rownames(.y)])  ,
+              covar = map2(covar, Y , ~.x[intersect(.x%>%rownames,rownames(.y)),]), # remove the dropped samples from Y
+              X_data = map(covar,~ filter_X( geno$bed[intersect(rownames(.x),rownames(geno$bed)),], imiss_cutoff, max(maf_cutoff, mac_cutoff/(2*length(intersect(rownames(.x),rownames(geno$bed))) ) ))   ))   # Y ( cov )  and specific X and covar match, filter X variants based on the overlapped samples.
+              #### Get residue Y for each of condition
 
-    ### Load phenotype and covariates
-    covar_list = covariate%>%mutate(covar = map(path, ~read_delim(.x,"\t")%>%select(-`#id`)%>%na.omit%>%t()))
-
+   phenotype_list = phenotype_list%>%mutate(Y_resid = map2(Y,covar,~.lm.fit(x = cbind(1,.y), y = .x)$residuals%>%
+                                                           scale%>%t%>%as_tibble)) ## T so that it can be unnest
+    if(y_as_matrix){
+    Y_resid = phenotype_list%>%select(Y_resid)%>%tidyr::unnest(Y_resid)%>%t%>%as.matrix
+    }else{Y_resid = map(phenotype_list$Y_resid,~.x%>%t) # Transpose back 
+    }
+    
+    maf_cutoff = max(maf_cutoff,mac_cutoff/(2*nrow(geno$fam)))
+    X = filter_X(geno$bed, imiss_cutoff, maf_cutoff) ## Filter X for mvSuSiE
+    print(paste0("Dimension of input genotype data is row:", nrow(X), " column: ",ncol(X) ))
+    
+    X_list = phenotype_list%>%mutate( X_resid = map2(X_data,covar,~.lm.fit(x = cbind(1,.y), y = .x)$residuals%>%scale))%>%pull(X_resid)
+    return (list(
+            residual_Y_scaled = Y_resid, # if y_as_matrix is true, then return a matrix of R conditions, with column names being the names of the conditions (phenotypes) and row names being sample names. Even for one condition it has to be a matrix with just one column. if y_as_matrix is false, then return a list of y either vector or matrix (CpG for example), and they need to match with residual_X_scaled in terms of which samples are missing.
+            residual_X_scaled = X_list , # is a list of R conditions each is a matrix, with list names being the names of conditions, column names being SNP names and row names being sample names
+            X = X ,  # is the somewhat original genotype matrix output from `filter_X`, with column names being SNP names and row names being sample names. Sample names of X should match example sample names of residual_Y_scaled matrix form (not list); but the matrices inside residual_X_scaled would be subsets of sample name of residual_Y_scaled matrix form (not list).
+            dropped_sample = phenotype_list$dropped_sample , # A documentation of dropped samples
+            traits = phenotype_list$phenotype_path
+            ))
+    }
+               
     # FIXME: 
     ### First, create the phenotype Y matrix
     ### with column names being the names of the conditions (phenotypes) and row names being sample names. Even for one condition it has to be a matrix with just one column
     ### NOTE: natually, missing data in Y should be allowed because as a matrix, some columns of Y may not have data for some samples and that is totally okay
-    traits = phenotype_list$tissue
+    
     
     ### Secondly, find matching samples between phenotype Y matrix, list of covariate matrices Z, and genotype X matrix, 
     ### and actually match the samples by name which should be the rownames
@@ -136,29 +165,10 @@ load_regional_association_data <- function(genotype, # PLINK file
     ### Each column of Y should match the corresponding Z matrix -- if the sample in the column does not exist in the corresponding Z matrix we have to set it NA; then we only keep the rows of Y that has at least non-NA values
     ### Hao, it is not clear to me if it is the case below because am not sure if the 3rd `mutate` assigns the variables sequentially
     ### So I'm not sure if the logic is 100% correct
-    phenotype_list = inner_join(phenotype,covar_list, by = "tissue")
-    phenotype_list = phenotype_list %>% mutate(flag = map(path.x, read_gene_pheno)) %>% dplyr::filter(lengths(flag) >1 ) %>% mutate(Y = map2(path.x,covar, ~read_gene_pheno(.x)%>%select(-c(`#chr`,start,end,${id_name}),      rownames(.y))%>%t%>%as.matrix))%>%
-                                    mutate(covar = map2(covar, Y , ~.x[intersect(.x%>%rownames,rownames(.y)),] ), # add a flag so that those returning NA can be filtered out
-                                            X_data = map(covar,~X[intersect(rownames(.x),rownames(X)),]),
-                                            covar = map2(covar, X_data , ~.x[intersect(.x%>%rownames,rownames(.y)),] ),
-                                            Y = map2(covar, Y ,~ .y[intersect(.x%>%rownames,rownames(.y)),] ),
-                                            #### Get residue for each of tissue
-                                            Y_resid = map2(Y,covar,~.lm.fit(x = cbind(1,.y), y = .x)$residuals%>%scale%>%t%>%as_tibble))
+   
     ### Next, regress covariate from each column of Y separately, take residual and center/scale it
-    Y_resid = phenotype_list%>%select(Y_resid)%>%tidyr::unnest(Y_resid)%>%t%>%as.matrix # in this part unnest will broad those matrix at smalled dimension (fewer rows/columns), so we should leave out those rows/ columns
-    colnames(Y_resid) = phenotype_list$tissue
-    ### Next, filter X matrix based on the remaining sample genotypes
-    N = nrow(X)
-    maf_cutoff = max(mac_cutoff/(2.0*N), maf_cutoff)
-    X = filter_X(X, imiss_cutoff, maf_cutoff)
+       ### Next, filter X matrix based on the remaining sample genotypes
+
     ### FIXME: complete this -- let's just summarize genotype data is enough
-    print("Dimension of filtered genotype data is")
     ### Finally, regress covariate from X to create a list of X residual matrices
-    X_list = phenotype_list%>%mutate( X_resid = map2(X_data,covar,~.lm.fit(x = cbind(1,.y), y = .x)$residuals%>%scale))%>%pull(X_resid)
     ## FIXME: return quantities should be these:
-    return (list(
-            residual_Y_scaled =, # if y_as_matrix is true, then return a matrix of R conditions, with column names being the names of the conditions (phenotypes) and row names being sample names. Even for one condition it has to be a matrix with just one column. if y_as_matrix is false, then return a list of y either vector or matrix (CpG for example), and they need to match with residual_X_scaled in terms of which samples are missing.
-            residual_X_scaled =, # is a list of R conditions each is a matrix, with list names being the names of conditions, column names being SNP names and row names being sample names
-            X = # is the somewhat original genotype matrix output from `filter_X`, with column names being SNP names and row names being sample names. Sample names of X should match example sample names of residual_Y_scaled matrix form (not list); but the matrices inside residual_X_scaled would be subsets of sample name of residual_Y_scaled matrix form (not list).
-            ))
-}
